686603df3160669591d6795b46303f125676599c
evadalli <evadalli@gmail.com>
2023-11-04 15:13:55 +0100

    backend: dbinit

---
+++ backend/dbinit.js
@@ -0,0 +1,30 @@
+/*migracios script irasa*/
+
+import sqlite3 from "sqlite3";
+
+/*adatbazis kapcsolatot reprezentalo objektum az sqlite3 kommunikáciojara az adatbazissal*/
+
+const db = new sqlite3.Database("webshop.db");
+
+db.serialize(() => {
+
+    /* sema elokeszitese ~ migracio */
+    /* primary key: ellenorzi, hogy ugyanaz az id nem lehet ketszer */
+    /* autoincrement: ha az id egesz szam, akkor nem kell megadni uj sornal, mert automatikusan kiosztja a kovetkezo id-t */
+
+    db.run(`CREATE TABLE products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name VARCHAR(80),
+        price INTEGER
+    )`);
+
+    /* kezdoadatok betoltese ~ seedelese */
+    db.run(`INSERT INTO products (name, price) VALUES ("laterator", 4200)`);
+    db.run(`INSERT INTO products (name, price) VALUES ("anterior foo", 5500)`);
+    db.run(`INSERT INTO products (name, price) VALUES ("posterior bar", 7000)`);
+})
+
+/* teszt: letrejon-e a webshop.db, migracios script lefuttatasa
+parancssor: node dbinit.js*/
+
+/*download sqlite command-line tools, open sqlite, command-line: .open webshop.db, .tables*/
