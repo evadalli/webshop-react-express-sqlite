ae3adb26fe65ed97d9375db85738cfda67007b91
evadalli <evadalli@gmail.com>
2023-11-05 18:13:06 +0100

    backend: restful status code and response + validation

--- backend/index.js
+++ backend/index.js
@@ -28,27 +28,63 @@ app.listen(8000);
 // vegpontok bekotese - request handler
 app.get("/products", (request, response) => {
     const products = [];
-
     // lekerdezes lefuttatasa, cb function utasitasanak vegrehajtasa minden sorra
     db.each(
       "SELECT * FROM products",
       (err, row) => {
-        products.push(row);
-      }, () => response.send(JSON.stringify(products))
+        products.push(row)
+      }, (err, rowCount) => {
+        let statusCode = 200;
+        let errorMessage = "";
+        if(err){
+          //err object tartalmazhat erzekeny adatokat, ezert ne azt adjuk at a frontendnek
+          errorMessage = "database error";
+          statusCode = 500;
+        }
+        response.status(statusCode).send(JSON.stringify({err: errorMessage, data: products}))}
     );
 });
 // git commmit: backend: get /products
 
 //request objektum body kulcsan talalhato objektum ~ termek objektum kulcsai
 app.post("/products", (request, response) => {
+  let statusCode = 200;
+  let errorMessage = ""
+  let responseBody = { name: request.body.name, price: request.body.price }
   
+  //validalas
+  // if + return = guard clause: ha nem teljesul az elofeltetel (precondition), akkor nem engedi tovabb a vegrehajtast
+  if(typeof request.body.name != "string" 
+  || request.body.name.length > 80 
+  || request.body.name == "" 
+  || typeof request.body.price != "number" 
+  || request.body.price < 0) {
+    errorMessage = "validation error";
+    statusCode = 400;
+    response
+      .status(statusCode)
+      .send(JSON.stringify({ err: errorMessage, data: responseBody }));
+      return;
+  }
   //sql injection megelozese erdekeben prepare statement-et hasznalunk kozvetlen sql futtatas helyett
   // - fontos, hogy ne konkatenaljuk be a parametereket
   // - hanem a preare statement-re hagyjuk ra a parameterek illeszteset
   // - kulonben ugyanugy erzekeny lesz sql injection-re
   const stmt = db.prepare(`INSERT INTO products (name, price) VALUES (?, ?)`);
-  stmt.run(request.body.name, request.body.price, (err) => {
-    response.send("OK")
+
+  // itt a callback function nem lehet arrow function, csak hagyomanyos (mert egyebkent a this nem jon letre) 
+  stmt.run(request.body.name, request.body.price, function (err) {
+    if(err) {
+      errorMessage = "database error";
+      statusCode = 500;
+      responseBody = { };
+    } else {
+
+      // a this egy statement tipusu object, aminek 2 property-je van, elso a lastID (amit kapott a felvitt objektum)
+      // masodik a changes (hany modositas lett, pl.: forEach-en belul tobb sor is lehet erintett)
+      responseBody.id = this.lastID;
+    }
+    response.status(statusCode).send(JSON.stringify( { err: errorMessage, data: responseBody }))
   } );
 
 })
